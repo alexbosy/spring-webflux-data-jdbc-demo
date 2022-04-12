a252a313b70461056b54c167e3f85ed1e2fb3665
Alexander Bosy <alex.bosy@gmail.com>
2022-04-12 22:27:28 +0300

    added spock support and sample unit test. added sample health check endpoint.

---
+++ src/test/groovy/io/cryptorush/userservice/HealthCheckControllerSpec.groovy
@@ -0,0 +1,10 @@
+package io.cryptorush.userservice
+
+import spock.lang.Specification
+
+class HealthCheckControllerSpec extends Specification {
+    def "dummy test"() {
+        expect:
+        1 == 1
+    }
+}
