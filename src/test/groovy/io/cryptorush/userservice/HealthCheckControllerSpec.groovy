bff14291bba95d90b3ffbe0c03ce57becf2c0072
Alexander Bosy <alex.bosy@gmail.com>
2022-11-17 18:02:40 +0200

    minor changes.

--- src/test/groovy/io/cryptorush/userservice/HealthCheckControllerSpec.groovy
+++ src/test/groovy/io/cryptorush/userservice/HealthCheckControllerSpec.groovy
@@ -3,6 +3,7 @@ package io.cryptorush.userservice
 import spock.lang.Specification
 
 class HealthCheckControllerSpec extends Specification {
+
     def "dummy test"() {
         expect:
         1 == 1
