5733123a611ff6b30dc85fba42276cd5ce4d0a18
Alexander Bosy <alex.bosy@gmail.com>
2022-11-23 23:45:46 +0200

    POST /users endpoint - changed errors rendering in request data validation.

--- src/main/java/io/cryptorush/userservice/rest/validation/ValidationHandler.java
+++ src/main/java/io/cryptorush/userservice/rest/validation/ValidationHandler.java
@@ -1,24 +1,26 @@
 package io.cryptorush.userservice.rest.validation;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
 
-import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ValidationHandler {
 
     @ExceptionHandler(WebExchangeBindException.class)
-    public ResponseEntity<List<String>> handleException(WebExchangeBindException e) {
+    public ResponseEntity<Map<String, String>> handleException(WebExchangeBindException e) {
         var errors = e.getBindingResult()
                 .getAllErrors()
                 .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
+                .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(), objectError -> {
+                    var msg = objectError.getDefaultMessage();
+                    return msg != null ? msg : "Something went wrong";
+                }));
         return ResponseEntity.badRequest().body(errors);
     }
 }
