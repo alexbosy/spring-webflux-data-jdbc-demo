4d268be7f6d007445261286381d53a436b51ff39
Alexander Bosy <alex.bosy@gmail.com>
2022-12-05 19:27:08 +0200

    defined ForwardedHeaderTransformer bean.

--- src/main/java/io/cryptorush/userservice/WebFluxConfig.java
+++ src/main/java/io/cryptorush/userservice/WebFluxConfig.java
@@ -1,10 +1,17 @@
 package io.cryptorush.userservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
 @Configuration
 @EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Bean
+    public ForwardedHeaderTransformer forwardedHeaderTransformer() {
+        return new ForwardedHeaderTransformer();
+    }
 }
